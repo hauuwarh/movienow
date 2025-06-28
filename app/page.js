5cb56442e3194ff7f4d9b918e8802c37135a7242
hauuwarh <amirahauwali16@gmail.com>
2025-06-28 14:53:14 +0100

    setting up our project

---
+++ app/page.js
@@ -0,0 +1,8 @@
+import { Main } from "next/document";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="font-bold text-center text-orange-400 text-4xl">Welcome to MovieNow</main>
+  );
+}
